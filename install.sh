5bfa3b23f96c549ab34949e2ac30da2cdb38b2a7
loidor <erik.ohrn@gmail.com>
2018-10-03 11:43:56 +0200

    Added priv lines.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+  echo "Måste köras som root"
+  exit 1
+fi
+
 cp restore /usr/local/bin/restore
 cp backup /usr/local/bin/backup
 chmod +x /usr/local/bin/restore
 chmod +x /usr/local/bin/backup
 echo "session-cleanup-script = /usr/local/bin/restore" >> /etc/lightdm/lightdm.conf
+
